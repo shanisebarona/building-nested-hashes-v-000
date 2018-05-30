144e959e1d3ea2562671a0e61f974ea8cd14d7f8
Shanise Barona <shanisebarona@gmail.com>
2018-05-30 22:23:35 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -24,7 +24,7 @@ def third_challenge
       :matriarch => {
         :name => "Lady Capulet",
         :age => "51"
-        },
+        }
       :heroine => {
         :name => "Juliet",
         :age => "15",

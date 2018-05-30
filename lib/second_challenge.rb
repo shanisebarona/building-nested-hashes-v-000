4d174e5bb387ba252c9cf56e6e1b3ffdfceccbb9
Shanise Barona <shanisebarona@gmail.com>
2018-05-30 22:10:16 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,10 +1,12 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      :patriarch
-      :matriarch
-      :hero
-      :hero_friends
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
       }, 
     :capulet => {
-      #your key/value pairs here!
+      
+    }
+  }
